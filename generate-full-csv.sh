04422d184f59471352b4631a06cd477bbb001d68
Adeel Kazmi <adeel.h.kazmi@gmail.com>
2019-12-28 04:01:16 +0000

    (generate-full-csv.sh) Removed redundant variables

--- generate-full-csv.sh
+++ generate-full-csv.sh
@@ -5,10 +5,6 @@ urls="$(cat $FILE)"
 for url in $urls
 do
   full=$(echo $url | cut -d/ -f 5-7 )
-  year=$(echo $full | cut -d/ -f 1)
-  month=$(printf "%02d" $(echo $full | cut -d/ -f 2))
-  day=$(printf "%02d" $(echo $full | cut -d/ -f 3))
-  date="${year}-${month}-${day}"
   command="python3 gen-csv.py $url"
   output=$(eval $command)
   echo "$full,$output"
