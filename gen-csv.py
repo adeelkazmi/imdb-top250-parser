9e6b666f191b6de742dd9b80f9d5fdfb4cd8e717
Adeel Kazmi <adeel.h.kazmi@gmail.com>
2020-01-05 22:58:28 +0000

    (gen-csv.py) Use MAX_FILMS to specify max films per URL

--- gen-csv.py
+++ gen-csv.py
@@ -3,6 +3,11 @@
 from bs4 import BeautifulSoup
 import check_url
 
+import os
+
+# Check if the MAX_FILMS environment variable is set and use it
+filmsToCollect = int( os.environ.get( 'MAX_FILMS', '251' ) )
+
 # Load the page with soup
 soup = BeautifulSoup(check_url.response.text, 'lxml')
 
@@ -21,11 +26,15 @@ for table in tables:
 # Iterate through all the rows in the table
 fullStr=""
 rows = tables[largestTable].findChildren(['tr'])
+rowsCollected=0
 for row in rows:
 	cols = row.find_all('td')
 	cols = [x.text.strip() for x in cols]
 	if cols:
 		fullStr=fullStr + cols[3].replace(',','') + ","
+		rowsCollected = rowsCollected + 1
+		if rowsCollected >= filmsToCollect:
+			break
 
 print( fullStr )
 
